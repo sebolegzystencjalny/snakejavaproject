f49dd68d9523be9d232533101111e94bc6540947
Sebastian Żółkiewicz <s.d.zolkiewicz@gmail.com>
2023-05-29 21:59:14 +0200

    begining of creating interface

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,8 @@
 //import static GameBoard.BOARD_HEIGHT;
 //import static GameBoard.BOARD_WIDTH;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
@@ -9,22 +12,34 @@ import javax.swing.JFrame;
 
 public class SnakeGame extends JFrame {
 //    gameBoard gameBoard
+    private JPanel menuView;
+    private JPanel GameView;
+    private JPanel CreateView;
+    private JPanel mainPanel;
     public SnakeGame() {
-        this.add(new GameBoard());
+        this.setBackground(Color.DARK_GRAY);
+        this.setFocusable(true);
+        this.set
+//        this.add(new GameBoard());
+        mainPanel = new JPanel();
+        this.add(mainPanel);
+        menuView = new MenuView();
+       
+        mainPanel.add(menuView, "menuView");
         this.setTitle("Snake Game");
-//        this.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
-//        this.setBackground(Color.DARK_GRAY);
-//        this.setFocusable(true);
-//        this.addKeyListener(new KeyAdapter() {
-//            @Override
-//            public void keyPressed(KeyEvent e) {
+        this.setPreferredSize(new Dimension(800, 500));
+        this.setBackground(Color.DARK_GRAY);
+        this.setFocusable(true);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
 //                if (inGame) {
-//                    gameState.addInput(e.getKeyCode());
+//                    //gameState.addInput(e.getKeyCode());
 //                } else {
-//                    initiateGame();
+//                    //initiateGame();
 //                }
-//            }
-//        });
+            }
+        });
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
